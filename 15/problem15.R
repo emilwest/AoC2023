be41d9e701ccfd4caf22c831bd107d3daee179de
Emil Westin <emilwestin@gmail.com>
2023-12-15 23:01:19 +0100

    d15

--- 15/problem15.R
+++ 15/problem15.R
@@ -45,14 +45,83 @@ calc_hash <- function(.chars) {
 
 summa <- chars |>
   str_split("") |>
-  map(calc_hash) |>
-  unlist() |> sum()
+  map_dbl(calc_hash) |>
+  sum()
 summa
 
 
 # part 2
 
 
-chars |>
-  str_split("")
+xx <- chars |>
+  enframe() %>%
+  mutate(token = str_extract(value, "^([a-z]+)(=|-)", group = 1 ),
+         operator = str_extract(value, "(=|-)"),
+         number = str_extract(value, "\\=([0-9]+)$", group = 1),
+         operator_meaning = ifelse(operator == "=", "add focal length", "remove lens"),
+         box = token %>% str_split("") %>% map_dbl(calc_hash)
+         )
 
+# pre-allocate output vectors/matrices
+n_boxes <- 256
+boxes <- vector("list", length=n_boxes)
+X <- matrix(nrow=0, ncol=2)
+colnames(X) <- c("lab", "length")
+for (i in 1:n_boxes) {
+  boxes[[i]] <- X
+}
+
+
+xx
+#i <- 151
+for (i in seq_len(nrow(xx))) {
+  tmp <- xx[i,]
+  b <- as.numeric(tmp$box)
+
+  currbox <- boxes[[b+1]]
+  currbox <- matrix(currbox, ncol=2)
+  colnames(currbox) <- c("label", "length")
+
+  if (tmp$operator == "=") {
+    isempty <- all(is.na(currbox))
+
+    if (!isempty & any(currbox[,1] == tmp$token)) {
+      # replace length
+      currbox[which(currbox[,1] == tmp$token), 2] <- tmp$number
+    } else {
+      currbox <- rbind(currbox, matrix(c(tmp$token, tmp$number), ncol=2))
+    }
+
+    boxes[[b+1]] <- currbox
+  }
+  if (tmp$operator == "-") {
+    # check if label exist in box
+    if (any(currbox[,1] == tmp$token)) {
+      if (!is.matrix(currbox)) stop("nej1")
+      remove_ind <- which(currbox[,1] == tmp$token)
+      currbox <- currbox[-remove_ind, ]
+      currbox <- matrix(currbox, ncol=2)
+      colnames(currbox) <- c("label", "length")
+      if (!is.matrix(currbox)) stop("nej2")
+      boxes[[b+1]] <- currbox
+    }
+
+
+  }
+
+}
+
+boxes
+
+map(boxes, ~as_tibble(.x) %>% mutate(across(everything(), as.character))) %>%
+  bind_rows(.id = "box") %>%
+  group_by(box) %>%
+  mutate(slot = row_number()) %>%
+  ungroup() %>%
+  mutate(
+    across(-label, as.numeric),
+    product = box*slot*length) %>%
+  summarise(summa = sum(product, na.rm=T))
+
+
+# 248279
